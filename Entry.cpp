6f83b0e26d8c68fe7ad8e7eefe88a092f2a4d067
Ray Zhang <41682958+zhang-ray@users.noreply.github.com>
2018-08-11 09:10:13 +0800

    Try to use chrono

--- Entry.cpp
+++ Entry.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <chrono>
 
-int main(void){
+int main(void) {
+
+    {
+        std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
+        std::time_t now_c = std::chrono::system_clock::to_time_t(now);
+        std::cout << "The time was "
+                  << std::put_time(std::localtime(&now_c), "%F %T") << '\n';
+    }
+
     for (int i = 0; i < 10; i++) {
       std::cout << "what's the matter?" << std::endl;
     }
+
     return 0;
 }
